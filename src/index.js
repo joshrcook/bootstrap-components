f68efeaafcfd0b900ebc8181f43d3dec529c52a0
joshrcook <joshua.cook@snapav.com>
2020-03-30 09:33:41 -0400

    updated index exports

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 export { default as Avatar } from './Avatar';
 export { default as Badge } from './Badge';
+export { default as Banner } from './Banner';
 export { default as Button } from './Button';
+export { default as Section } from './Section';
 export { default as Type } from './Type';
