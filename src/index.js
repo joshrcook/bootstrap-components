86243ab6068fc545105624aa472d2956df3df6f3
joshrcook <joshua.cook@snapav.com>
2020-04-06 16:33:04 -0400

    First build

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 export { default as Avatar } from './Avatar';
+export { default as Background } from './Background';
 export { default as Badge } from './Badge';
 export { default as Banner } from './Banner';
 export { default as Button } from './Button';
-export { default as Section } from './Section';
+export { default as Col } from './Col';
+export { default as Container } from './Container';
+export { default as Icon } from './Icon';
+export { default as Row } from './Row';
+export { default as ThemeProvider } from './ThemeProvider';
 export { default as Type } from './Type';
