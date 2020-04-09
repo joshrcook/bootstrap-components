8aa2a0cb42e7cb2f1e7ca4cacd8d01ff604260ee
joshrcook <joshua.cook@snapav.com>
2020-04-09 17:14:36 -0400

    Adding in the tailwind ui marketing theme

---
+++ .storybook/tailwind-ui-marketing-theme/main.js
@@ -0,0 +1,8 @@
+const defaultConfig = require('../main');
+
+export default {
+    ...defaultConfig,
+    stories: [
+        '../../src/**/*.tui-marketing.stories.js',
+    ],
+};
---
+++ .storybook/tailwind-ui-marketing-theme/preview.js
@@ -0,0 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { addDecorator } from '@storybook/react';
+import { withKnobs } from '@storybook/addon-knobs';
+
+
+// Add knobs globally
+addDecorator(withKnobs);
